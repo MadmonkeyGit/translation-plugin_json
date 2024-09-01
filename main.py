ffbcda7a4343f5cea059ea1ba46b24833156fef7
MadMonkeyGit <leonhard.raschein@gmail.com>
2024-09-01 18:44:16 +0200

    update

--- main.py
+++ main.py
@@ -3,33 +3,53 @@ import os
 import json
 from deepdiff import DeepDiff
 
-def add_missing_keys(master_file: dict, slave_file) -> None:
-    diff = DeepDiff(master_file, slave_file, ignore_order=False)
 
-    # print(diff['dictionary_item_removed'])
+def translate_str(txt: str) -> str:
+    txt_translated = "---translated"
+    return txt_translated
+
+
+def translate(txt: str | dict) -> str | dict:
+    if type(txt) == str:
+        txt_translated = translate_str(txt)
+    else:
+        txt_translated = txt
+        for key in list(txt.keys()):
+            txt_translated[key] = translate_str(txt[key])
+    return txt_translated
+
+
+def add_missing_keys(master_file: dict, slave_file: dict) -> dict:
+    diff = DeepDiff(master_file, slave_file, ignore_order=False)
+    slave_file_tmp = slave_file
     if 'dictionary_item_removed' in diff:
         for idx, value in enumerate(diff['dictionary_item_removed']):
-            # print(f"{idx = }: {value}")
             key_list = value.split('[')[1:]
             for i in range(len(key_list)):
                 key_list[i] = key_list[i].strip("']")
 
             if len(key_list) == 1:
-                slave_file[key_list[0]] = master_file[key_list[0]]
+                pos = list(master_file.keys()).index(key_list[0])
+                items = list(slave_file_tmp.items())
+                items.insert(pos, (key_list[0], translate(master_file[key_list[0]])))
+                slave_file_tmp = dict(items)
+
+            elif len(key_list) == 2:
+                pos = list(master_file[key_list[0]].keys()).index(key_list[1])
+                items = list(slave_file_tmp[key_list[0]].items())
+                items.insert(pos, (key_list[1], translate(master_file[key_list[0]][key_list[1]])))
+                slave_file_tmp[key_list[0]] = dict(items)
+
             else:
-                slave_file[key_list[0]][key_list[1]] = master_file[key_list[0]][key_list[1]]
-
-
-            # print(f"{key_list = }")
-            # print(f"{master_file[key_list[0]][key_list[1]] = }")
-    # print(f"{slave_file = }")
+                raise Exception(f"Depth of json-file is maximum two, now it's: {len(key_list)}")
+    return slave_file_tmp
 
 
 def main() -> None:
     path_resources = os.path.join(os.getcwd(), "resource")
     name_main_file = "de.json"
     translation_file_names = ["en.json", "fr.json", "it.json"]
-    translation_files = [dict] * len(translation_file_names)
+    translation_files = [""] * len(translation_file_names)
 
     with open(os.path.join(path_resources, name_main_file), 'r') as file:
         main_file = json.load(file)
@@ -37,7 +57,7 @@ def main() -> None:
     for idx, file_name in enumerate(translation_file_names):
         with open(os.path.join(path_resources, file_name), 'r') as file:
             translation_files[idx] = json.load(file)
-        add_missing_keys(main_file, translation_files[idx])
+        translation_files[idx] = add_missing_keys(main_file, translation_files[idx])
         print(translation_files[idx])
 
 
