8523084df315d10df96ed26cd4913527aec24123
MadMonkeyGit <leonhard.raschein@gmail.com>
2024-09-13 20:36:24 +0200

    almost done

--- main.py
+++ main.py
@@ -2,20 +2,16 @@ import time
 import os
 import json
 from deepdiff import DeepDiff
+from deep_translator import GoogleTranslator
 
 
-def translate_str(txt: str) -> str:
-    txt_translated = "---translated"
-    return txt_translated
-
-
-def translate(txt: str | dict) -> str | dict:
-    if type(txt) == str:
-        txt_translated = translate_str(txt)
-    else:
+def translate(txt: str | dict, target_language: str) -> str | dict:
+    if type(txt) == dict:
         txt_translated = txt
         for key in list(txt.keys()):
-            txt_translated[key] = translate(txt[key])
+            txt_translated[key] = translate(txt[key], target_language)
+    else:
+        txt_translated = GoogleTranslator(source='auto', target=target_language).translate(txt)
     return txt_translated
 
 
@@ -46,7 +42,7 @@ def nested_dict_delete(dic: dict, keys: list):
     del dic[keys[-1]]
 
 
-def add_missing_keys(master_file: dict, slave_file: dict) -> dict:
+def add_missing_keys(master_file: dict, slave_file: dict, target_language: str) -> dict:
     diff = DeepDiff(master_file, slave_file, ignore_order=False)
     if 'dictionary_item_removed' in diff:
         for idx, value in enumerate(diff['dictionary_item_removed']):
@@ -54,7 +50,7 @@ def add_missing_keys(master_file: dict, slave_file: dict) -> dict:
             for i in range(len(key_list)):
                 key_list[i] = key_list[i].strip("']")
             nested_dict_add_key(slave_file, key_list)
-            nested_dict_set(slave_file, key_list, translate(nested_dict_get(master_file, key_list)))
+            nested_dict_set(slave_file, key_list, translate(nested_dict_get(master_file, key_list), target_language))
     if 'dictionary_item_added' in diff:
         for idx, value in enumerate(diff['dictionary_item_added']):
             key_list = value.split('[')[1:]
@@ -67,7 +63,7 @@ def add_missing_keys(master_file: dict, slave_file: dict) -> dict:
             for i in range(len(key_list)):
                 key_list[i] = key_list[i].strip("']")
             nested_dict_delete(slave_file, key_list)
-            nested_dict_set(slave_file, key_list, translate(nested_dict_get(master_file, key_list)))
+            nested_dict_set(slave_file, key_list, translate(nested_dict_get(master_file, key_list), target_language))
     return slave_file
 
 
@@ -82,9 +78,20 @@ def main() -> None:
     print(f"{main_file = }")
 
     for idx, file_name in enumerate(translation_file_names):
+        if "en" in file_name:
+            language = "en"
+        elif "fr" in file_name:
+            language = "fr"
+        elif "it" in file_name:
+            language = "it"
+        else:
+            language = "de"
+        print(f"{language = }")
         with open(os.path.join(path_resources, file_name), 'r') as file:
             translation_files[idx] = json.load(file)
-        translation_files[idx] = add_missing_keys(main_file, translation_files[idx])
+        translation_files[idx] = add_missing_keys(main_file, translation_files[idx], language)
+        with open(os.path.join(path_resources, file_name), "w") as outfile:
+            json.dump(translation_files[idx], outfile)
         print(f"translation_files[{idx}] = {translation_files[idx]}")
 
 
